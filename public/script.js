495440391d7b2889e0ccacaf6fe4c51d340c601e
LoudLunatics <nauvalazfa@proton.me>
2026-01-12 13:53:02 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -43,6 +43,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   ];
 
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  shuffleArray(questions);
+
   let current = 0;
   let isLocked = false;
 
